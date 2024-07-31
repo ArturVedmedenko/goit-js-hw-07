690664378567883f41ef06ec30fdd32049112d4e
Artur Vedmedenko <vedmedenko.a@gmail.com>
2024-07-31 17:33:34 +0300

    add task-2 and task-3

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach(category => {
   const categoryTitle = category.querySelector('h2').textContent;
-  const categoryItems = category.querySelectorAll('ul li').length;
+  const categoryItems = category.querySelectorAll('li').length;
   console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${categoryItems}`);
 });
